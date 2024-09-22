5ab5a890e45b8a4073c7b13c86a6be3631beb06b
MohamedWael21 <mohame171819@gmail.com>
2024-09-23 00:53:54 +0300

    modify starter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
+import createHttpError from "http-errors";
+import globalErrorHandler from "./utils/global_error_handler";
+import { catchAsyncError } from "./utils/helpers";
+
+
+
 
 const app = express();
 
-app.use(morgan("tiny"));
+app.use(helmet());
 
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+  
+
+app.use(
+    catchAsyncError(async (req: ExpressRequest) => {
+      throw createHttpError.NotFound(`${req.path} resource doesn't exists`);
+    }),
+  );
+  
+app.use(globalErrorHandler);
+  
 
 export default app;
 
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,32 @@ import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
+
+const shutdown = async (signal: string) => {
+  console.log(`\nReceived ${signal}. Gracefully shutting down...`);
+
+  server.close((err) => {
+    if (err) {
+      console.error("Error closing Express server:", err);
+      process.exit(1);
+    }
+
+    console.log("Express server closed.");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+
+process.on("uncaughtException", (err) => {
+  console.log(err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
 });
 
